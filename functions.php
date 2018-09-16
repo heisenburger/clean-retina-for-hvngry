d57cfb5293431a80240468e6e3ad7ba5ff5b393f
Serena Chen <serenadantingchen@gmail.com>
2018-09-16 18:01:25 +1200

    use compass sass, style header

--- functions.php
+++ functions.php
@@ -1,19 +1,16 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'clean-retina-style';
+    $parent_style = 'clean-retina';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
+    wp_enqueue_style( 'clean-retina-for-hvngry',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-
-if ( ! isset( $content_width ) )
-  $content_width = 962;
 
 function wpsites_random_posts_order( $query ) {
     if ( $query->is_home() && !is_admin() && $query->is_main_query() ) {
